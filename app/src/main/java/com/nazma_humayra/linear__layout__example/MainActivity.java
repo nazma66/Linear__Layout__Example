8ddf81c58d975f6e07d48e6b003161b7837c19a9
Nazma Humayra <nazmahumayra.8499@gmail.com>
2024-01-15 11:26:56 +0600

    Initial commit

---
+++ app/src/main/java/com/nazma_humayra/linear__layout__example/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nazma_humayra.linear__layout__example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
